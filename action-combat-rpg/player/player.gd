e94ce2f376f433a44056ba83598b1560289687ed
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-11-17 20:43:38 -0800

    Add interpolation to movement

--- action-combat-rpg/player/player.gd
+++ action-combat-rpg/player/player.gd
@@ -9,7 +9,7 @@ var _look = Vector2.ZERO
 @export var mouse_sensitivity: float = 0.00075
 @export var min_boundary: float = -60
 @export var max_boundary: float = 10
-
+@export var animation_decay: float = 20.0
 
 @onready var horizontal_pivot: Node3D = $HorizontalPivot
 @onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
@@ -68,4 +68,7 @@ func look_towards_direction(direction: Vector3, delta: float) -> void:
 	var target_transform := rig_pivot.global_transform.looking_at(
 		rig_pivot.global_position + direction, Vector3.UP, true
 	)
-	rig_pivot.global_transform.basis = target_transform.basis
+	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(
+		target_transform,
+		1.0 - exp(-animation_decay * delta)
+	)
