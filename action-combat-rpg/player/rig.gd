ee64aebc2768fa2013b15354a5a2ea28e93f35b3
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-11-26 17:15:39 -0800

    Add animations to the tree

--- action-combat-rpg/player/rig.gd
+++ action-combat-rpg/player/rig.gd
@@ -1,11 +1,21 @@
 extends Node3D
 
+@export var animation_speed: float = 10.0
+
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 var run_path: String = "parameters/MoveSpace/blend_position"
+var run_weight_target := -1.0
+
+func _physics_process(delta: float) -> void:
+	animation_tree[run_path] = move_toward(
+		animation_tree[run_path],
+		run_weight_target,
+		delta * animation_speed
+	)
 
 func update_animation_tree(direction: Vector3) -> void:
 	if direction.is_zero_approx():
-		animation_tree[run_path] = -1.0
+		run_weight_target = -1.0
 	else:
-		animation_tree[run_path] = 1.0
+		run_weight_target = 1.0
