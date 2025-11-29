bae15d8c55975fff07864eb84174a3b15ed8db7e
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-11-28 16:56:26 -0800

    Add health component

--- action-combat-rpg/enemy/enemy.gd
+++ action-combat-rpg/enemy/enemy.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
+@export var max_health: float = 20.0
+
 @onready var rig: Node3D = $Rig
+@onready var health_component: HealthComponent = $HealthComponent
 
 func _ready() -> void:
 	rig.set_active_mesh(
 		rig.villager_meshes.pick_random()
 	)
+	health_component.update_max_health(max_health)
+	
