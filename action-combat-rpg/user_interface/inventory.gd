d56c6b3d91534192ecf5d87bd0c85f5239a7970e
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-12-02 21:36:37 -0800

    Add more inventory functionality

--- action-combat-rpg/user_interface/inventory.gd
+++ action-combat-rpg/user_interface/inventory.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var speed_value: Label = %SpeedValue
 @onready var endurance_value: Label = %EnduranceValue
 @onready var level_label: Label = %LevelLabel
+@onready var attack_value: Label = %AttackValue
 
 @onready var player: Player = get_parent().player
 
@@ -16,3 +17,15 @@ func update_stats() -> void:
 	agility_value.text = str(player.stats.agility.ability_score)
 	speed_value.text = str(player.stats.speed.ability_score)
 	endurance_value.text = str(player.stats.endurance.ability_score)
+	level_label.text = "Level %s" % player.stats.level
+
+func update_gear_stats() -> void:
+	attack_value.text = str(get_weapon_damage())
+
+func get_weapon_damage() -> int:
+	var damage = 10
+	damage += player.stats.get_damage_modifier()
+	return damage
+
+func _on_texture_button_pressed() -> void:
+	get_parent().close_menu()
