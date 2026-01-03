55d65c15a8f313ec49c216da63d18a3a371108b3
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2026-01-02 22:04:18 -0800

    Add equip items capabilities

--- action-combat-rpg/user_interface/inventory.gd
+++ action-combat-rpg/user_interface/inventory.gd
@@ -1,4 +1,5 @@
 extends Control
+class_name Inventory
 
 @onready var strength_value: Label = %StrengthValue
 @onready var agility_value: Label = %AgilityValue
@@ -6,6 +7,16 @@ extends Control
 @onready var endurance_value: Label = %EnduranceValue
 @onready var level_label: Label = %LevelLabel
 @onready var attack_value: Label = %AttackValue
+@onready var item_grid: GridContainer = %ItemGrid
+@onready var gold_label: Label = %GoldLabel
+@onready var weapon_slot: CenterContainer = %WeaponSlot
+@onready var shield_slot: CenterContainer = %ShieldSlot
+@onready var armor_slot: CenterContainer = %ArmorSlot
+
+@onready var gold := 0:
+	set(value):
+		gold = value
+		gold_label.text = str(gold) + "g"
 
 @onready var player: Player = get_parent().player
 
@@ -29,3 +40,24 @@ func get_weapon_damage() -> int:
 
 func _on_texture_button_pressed() -> void:
 	get_parent().close_menu()
+
+func add_item(icon: ItemIcon) -> void:
+	for connection in icon.interact.get_connections():
+		icon.interact.disconnect(connection.callable)
+	icon.get_parent().remove_child(icon)
+	item_grid.add_child(icon)
+	icon.interact.connect(interact)
+
+func add_currency(currency_in: int) -> void:
+	gold += currency_in
+
+func equip_item(item: ItemIcon, item_slot: CenterContainer) -> void:
+	for child in item_slot.get_children():
+		add_item(child)
+	item.get_parent().remove_child(item)
+	item_slot.add_child(item)
+
+func interact(item: ItemIcon) -> void:
+	if item is WeaponIcon:
+		equip_item(item, weapon_slot)
+	update_gear_stats()
--- action-combat-rpg/user_interface/loot_container_display.gd
+++ action-combat-rpg/user_interface/loot_container_display.gd
@@ -1,5 +1,7 @@
 extends CenterContainer
 
+@export var inventory: Inventory
+
 @onready var grid_container: GridContainer = $PanelContainer/VBoxContainer/GridContainer
 @onready var title_label: Label = %TitleLabel
 
@@ -13,6 +15,7 @@ func close() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if is_instance_valid(current_container):
 		for item in grid_container.get_children():
+			item.interact.disconnect(pickup_item)
 			grid_container.remove_child(item)
 			current_container.add_child(item)
 			item.visible = false
@@ -27,5 +30,14 @@ func open(loot: LootContainer) -> void:
 			current_container.remove_child(item)
 			grid_container.add_child(item)
 			item.visible = true
+			item.interact.connect(pickup_item)
 		visible = true
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func pickup_item(icon: ItemIcon) -> void:
+	icon.interact.disconnect(pickup_item)
+	if icon is CurrencyIcon:
+		inventory.add_currency(icon.value)
+		icon.queue_free()
+	else:
+		inventory.add_item(icon)
